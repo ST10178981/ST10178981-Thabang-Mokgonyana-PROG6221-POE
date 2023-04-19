2073acec640c27ec83883495d3ecc64acbd37a5e
ST1215024 <102739012+ST1215024@users.noreply.github.com>
2023-04-19 11:35:07 +0200

    Add project files.

---
+++ ST10178981 Thabang Mokgonyana PROG6221 POE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
