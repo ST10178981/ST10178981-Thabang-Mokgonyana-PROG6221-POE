0e36b4654cb783940e46b812624e06c38b923939
ST1215024 <102739012+ST1215024@users.noreply.github.com>
2023-04-26 18:52:08 +0200

    Created menu item 1 and 2

--- Program.cs
+++ Program.cs
@@ -47,7 +47,84 @@ namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
                 //if statement tied to above integer value to execute the desired menu option
                 if (function == 1)
                 {
+                    //internal coour of function changed to cyan
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    //Asking user for set value and calling external int to retrieve the value and store it
+                    Console.WriteLine("Enter the number of ingredients");
+                    adder.setingredientAmount(Convert.ToInt32(Console.ReadLine());
+
+                    //for loop iterting the desired output message prompting the user to enter values and storing them accoding to the above given value
+                    for (int i = 0; i < setingredientAmount; i++)
+                    {
+
+                        Console.WriteLine("Enter the name of the ingredient");
+                        
+                        ingredientNamesArr[i] = Console.ReadLine();
+
+
+                        Console.WriteLine("Enter the Quantity of the ingredient");
+
+                        ingredientQuantArr[i] = Convert.ToDouble(Console.ReadLine());
+
+
+                        Console.WriteLine("Enter the Unit of measurement(kg,ml,g,l)");
+
+                        ingredientUnitOfMeas[i] = Console.ReadLine();
+
+                    }
+
+                    //Changing colour once again to gray
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    //Asking user for set value and calling external int to retrieve the value and store it
+                    Console.WriteLine("Enter the number of steps for the recipe");
+
+                    adder.setrecipeStepsAmount(Convert.ToInt32(Console.ReadLine());
+
+                    for (int i = 0; i < adder.setrecipeStepsAmount(); i++)
+                    {
+
+                        Console.WriteLine("Enter description for Ingredient " + (i + 1));
+
+                        recipeStepsArr[i] = Console.ReadLine();
+                    }
+                } else if (function == 2)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    //Creating blank string value to store and display the string builder
+                    string Recipte = "";
                     
+                    for (int i = 0; i < adder.setingredientAmount(); i++)
+                    {
+                        if (ingredientNamesArr[i] == null && ingredientQuantArr[i] == null)
+                        {
+                            Console.WriteLine("No ingredients or recipes added!");
+                        }
+                        else
+                        {
+
+
+                            for (int i = 0; i < setingredientAmount(); i++)
+                            {
+
+                                showRecipe.Append("Item Name: " + ingredientNamesArr[i] + "\n" +
+                                "Item Quantity: " + ingredientQuantArr[i] + ingredientUnitOfMeas + "\n");
+                            }
+
+                            for (int i = 0; i < recipeStepsAmount; i++)
+                            {
+
+                                showRecipe.Append("Steps to make the recipe:\n" +
+                                "Step " + i + " : " + recipeStepsArr[i] + "\n");
+                            }
+                        }
+                    }
+
+                    Console.WriteLine(showRecipe.ToString());
+                    Console.ReadLine();
+
+                }
+                
                 }
                 
 
