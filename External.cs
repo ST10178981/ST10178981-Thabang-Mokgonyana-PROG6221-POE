770de76047ca9d0e1d4da93c29318b319d749d30
ST1215024 <102739012+ST1215024@users.noreply.github.com>
2023-05-28 10:54:20 +0200

    Added a list variable. Cleared incomplete function and added a for loop. fixed getters and setters. added variable get name.

--- External.cs
+++ External.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Xml.Linq;
 
 public class External
 {
@@ -6,55 +8,95 @@ public class External
 
     public External()
     {
+
+    }
+    
         //declaring private ints to call in the main method and solution
-        private int ingredientAmount;
+        private int ingredientSum;
         private int ingredientType;
-        private int scaleOption;
+        private int scaleChoice;
+        private int recipeList;
+        private int recipeStepsType;
+        private String name;
+
 
         //Method to retrieve value for private int 1 above
 
-        public void setIngredientAmount(int ingredientSum)
+        public void getIngredientAmount(int ingredientAmount)
         {
-            this.ingredientAmount = ingredientSum;
+            this.ingredientSum = ingredientAmount;
         }
-    
+
 
         //Returns and stores value for private int 1 from the main class
-        public int getIngredientAmount()
+        public int setIngredientAmount()
         {
-            return this.ingredientAmount
+            return this.ingredientSum;
         }
 
-    //Method to retrieve value for private int 2 above
-    public void setrecipeStepsAmount(int recipeList)
-    {
-        this.recipeStepsAmountt = recipeList;
-    }
-
-    //Returns and stores value for private int 1 from the main class
-    public int getIngredientAmount()
-    {
-        return this.recipeStepsAmount
+        //Method to retrieve value for private int 2 above
+        public void getrecipeStepsAmount(int recipeStepsAmount)
+        {
+            this.recipeList = recipeStepsAmount;
         }
 
-    //Method to retrieve value for private int 3 above
-    public void setScaleOption(int scaleChoice)
-    {
-        this.scaleOption = scaleChoice;
-    }
-
-    //Returns and stores value for private int 1 from the main class
-    public int getscaleOption()
-    {
-        return this.scaleOption
+        public int setrecipeStepsAmount()
+        {
+            return this.recipeList;
         }
 
+
+        //Method to retrieve value for private int 3 above
+        public void getScaleOption(int scaleOption)
+        {
+            this.scaleChoice = scaleOption;
+        }
+
+        //Returns and stores value for private int 1 from the main class
+        public int setscaleOption()
+        {
+            return this.scaleChoice;
+        }
+
+        public void recipeName(List <String> recipeNames, int recipeNameCount)
+        {
+        
+            for (int i = 0; i < recipeNameCount; i++)
+            {
+                Console.WriteLine("Please enter the name of the recipe");
+                recipeNames.Add(Console.ReadLine());
+            }
+        
+        }
+
+        //Unit of measurement enum
+        enum UnitOfMeasurement
+        {
+            Teaspoon,
+            Tablespoon,
+            Cup,
+            Gram,
+            Ounce,
+            Pound
+        }
+        //Food group enum
+        enum FoodGroup
+        {
+            Dairy,
+            Fruit,
+            Grain,
+            Protein,
+            Vegetable
+        }
+
+        public void setName(string character)
+        {
+        this.name = character;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
 }
-
-
-
-
-
-}
-
 
--- Program.cs
+++ Program.cs
@@ -1,28 +1,34 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml.Linq;
 
 namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
 {
     internal class Program {
 
         //Declaring values that will be used throughout the program
-        public static String[] ingredientNamesArr = new String[9];
-        public static String[] recipeStepsArr = new String[9];
-        public static double[] ingredientQuantArr = new double[9];
-        public static String[] ingredientUnitOfMeas = new String[9];
-        public static int ingredientAmount;
-        public static int recipeStepsAmount;
-        public static String recipeName;
-        public static int p;
-        double scaleIngrediants;
+        static String[] ingredientNamesArr = new String[9];
+        static String[] recipeStepsArr = new String[9];
+        static double[] ingredientQuantArr = new double[9];
+        static String[] ingredientUnitOfMeas = new String[9];
+        static int ingredientAmount;
+        static int recipeStepsAmount;
+        static int p;
+        static int scaleOption;
+        static double scaleIngrediants;
+        static string r;
+        static string character;
+        static List<String> recipeNames = new List<String>();
+        static int recipeNameCount;
+        static StringBuilder showRecipe = new StringBuilder();
         
-
 
 
 
         static void Main(string[] args)
         {
             //StringBuilder made to complie list for function 2
-            StringBuilder showRecipe = new StringBuilder();
+            
             //Object declared to call the external class
             External adder = new External();
             //value to hold while loop
@@ -31,10 +37,18 @@ namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
             //While loop to make sure the program runs until the exit option is choosen
             while (p != 6)
             {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("=====================================");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("WELCOME TO THE RECIPE LOGGING APPLICATION");
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("=====================================");
                 //Chnging menu colour to blue
-                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
                 //Console menu list
-                Console.WriteLine("Welcome! Please select a function below\n" +
+                Console.WriteLine("Please select a function below\n" +
                     "1. Record the ingredients and steps.\n" +
                     "2. Display the full recipe\n" +
                     "3. Scale the unit of measurement\n" +
@@ -50,78 +64,84 @@ namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
                     //internal coour of function changed to cyan
                     Console.ForegroundColor = ConsoleColor.Cyan;
                     //Asking user for set value and calling external int to retrieve the value and store it
-                    Console.WriteLine("Enter the number of ingredients");
-                    adder.setingredientAmount(Convert.ToInt32(Console.ReadLine());
-
-                    //for loop iterting the desired output message prompting the user to enter values and storing them accoding to the above given value
-                    for (int i = 0; i < setingredientAmount; i++)
+                    Console.WriteLine("Plaese enter the number of recipes you want to log");
+                    recipeNameCount = Convert.ToInt32(Console.ReadLine());
+                    adder.recipeName(recipeNames, recipeNameCount);
+                    foreach (String name in recipeNames)
                     {
+                        Console.WriteLine("Enter the number of ingredients");
+                        adder.getIngredientAmount(ingredientAmount);
+                        ingredientAmount = Convert.ToInt32(Console.ReadLine());
+                        adder.setIngredientAmount();
 
-                        Console.WriteLine("Enter the name of the ingredient");
-                        
-                        ingredientNamesArr[i] = Console.ReadLine();
+                        //for loop iterting the desired output message prompting the user to enter values and storing them accoding to the above given value
+                        for (int i = 0; i < ingredientAmount; i++)
+                        {
+
+                            Console.WriteLine("Enter the name of the ingredient");
+
+                            ingredientNamesArr[i] = Console.ReadLine();
 
 
-                        Console.WriteLine("Enter the Quantity of the ingredient");
+                            Console.WriteLine("Enter the Quantity of the ingredient");
 
-                        ingredientQuantArr[i] = Convert.ToDouble(Console.ReadLine());
+                            ingredientQuantArr[i] = Convert.ToDouble(Console.ReadLine());
 
 
-                        Console.WriteLine("Enter the Unit of measurement(kg,ml,g,l)");
+                            Console.WriteLine("Enter the Unit of measurement(kg,ml,g,l)");
 
-                        ingredientUnitOfMeas[i] = Console.ReadLine();
+                            ingredientUnitOfMeas[i] = Console.ReadLine();
 
+                        }
+
+                        //Changing colour once again to gray
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        //Asking user for set value and calling external int to retrieve the value and store it
+                        Console.WriteLine("Enter the number of steps for the recipe");
+
+                        adder.getrecipeStepsAmount(recipeStepsAmount);
+                        recipeStepsAmount = Convert.ToInt32(Console.ReadLine());
+                        adder.setrecipeStepsAmount();
+
+                        for (int i = 0; i < recipeStepsAmount; i++)
+                        {
+
+                            Console.WriteLine("Enter description for Ingredient " + (i + 1));
+
+                            recipeStepsArr[i] = Console.ReadLine();
+                        }
                     }
-
-                    //Changing colour once again to gray
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    //Asking user for set value and calling external int to retrieve the value and store it
-                    Console.WriteLine("Enter the number of steps for the recipe");
-
-                    adder.setrecipeStepsAmount(Convert.ToInt32(Console.ReadLine());
-
-                    for (int i = 0; i < adder.setrecipeStepsAmount(); i++)
-                    {
-
-                        Console.WriteLine("Enter description for Ingredient " + (i + 1));
-
-                        recipeStepsArr[i] = Console.ReadLine();
-                    }
+                    
+                   
                 } else if (function == 2)
                 {
 
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     //Creating blank string value to store and display the string builder
-                    string Recipte = "";
-                    
-                    for (int i = 0; i < adder.setingredientAmount(); i++)
+
+                    adder.setName(character);
+
+                    Console.WriteLine("-----Recipes-----");
+                    List<string> sortedRecipeNames = recipeNames.Select(r => adder.GetName()).OrderBy(name => name).ToList();
+                    foreach (string name in sortedRecipeNames)
                     {
-                        if (ingredientNamesArr[i] == null && ingredientQuantArr[i] == null)
-                        {
-                            Console.WriteLine("No ingredients or recipes added!");
-                        }
-                        else
-                        {
-
-
-                            for (int i = 0; i < setingredientAmount(); i++)
-                            {
-
-                                showRecipe.Append("Item Name: " + ingredientNamesArr[i] + "\n" +
-                                "Item Quantity: " + ingredientQuantArr[i] + ingredientUnitOfMeas + "\n");
-                            }
-
-                            for (int i = 0; i < recipeStepsAmount; i++)
-                            {
-
-                                showRecipe.Append("Steps to make the recipe:\n" +
-                                "Step " + i + " : " + recipeStepsArr[i] + "\n");
-                            }
-                        }
+                        Console.WriteLine(name);
                     }
-                    //Output of the strinbuilder
-                    Console.WriteLine(showRecipe.ToString());
-                    Console.ReadLine();
+
+                    Console.WriteLine("Enter the name of the recipe to display details (or press Enter to go back):");
+                    string recipeName = Console.ReadLine();
+                    if (string.IsNullOrEmpty(recipeName))
+                        return;
+
+                    External selectedRecipe = recipeNames.FirstOrDefault(r => r.GetName() == recipeName);
+                    if (selectedRecipe == null)
+                    {
+                        Console.WriteLine($"No recipe found with name: {recipeName}");
+                        return;
+                    }
+
+                    Console.WriteLine(selectedRecipe.ToString());
+
 
                 }
                 else if (function == 3)
@@ -132,25 +152,27 @@ namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
                         "1. O.5(Half)\n" +
                         "2. 2(Double)\n" +
                         "3. 3(Triple)");
-                    adder.scaleOption(int.Parse(Console.ReadLine());
+                    adder.getScaleOption(scaleOption);
+                    scaleOption = int.Parse(Console.ReadLine());
+                    adder.setscaleOption();
                     //For loop to change each value within the array ingrediantuantArr by the factor of the given option
-                    for (int i = 0; i < ingredientAmoun; i++)
+                    for (int i = 0; i < ingredientAmount; i++)
                     {
-                        if (adder.scaleOption == 1)
+                        if (scaleOption == 1)
                         {
                             // Halfing all ingrediantuantArr with confirmation message
-                            scaleIngrediants = ingredientQuantArr[i] * 0,5;
+                            scaleIngrediants = ingredientQuantArr[i] * 0.5;
                             Console.WriteLine("Scale has been halved");
                             Console.ReadLine();
                         }
-                        else if (adder.scaleOption == 2)
+                        else if (scaleOption == 2)
                         {
                             //Doubling all ingrediantuantArr with confirmation message
                             scaleIngrediants = ingredientQuantArr[i] * 2;
                             Console.WriteLine("Quantity has been doubled");
                             Console.ReadLine();
                         }
-                        else if (adder.scaleOption == 3)
+                        else if (scaleOption == 3)
                         {
                             //Tripling all ingrediantuantArr with confirmation message
                             scaleIngrediants = ingredientQuantArr[i] * 3;
@@ -164,43 +186,39 @@ namespace ST10178981_Thabang_Mokgonyana_PROG6221_POE
                             Console.WriteLine("Invalid input");
                         }
                     }
+                }
                 else if (function == 4)
                 {
-                        //CANNOT FIND A SOULTION 
-                        Console.ForegroundColor = ConsoleColor.White;
-                        for (int i = 0; i < ingredientQuantArr.Length; i++)
-                        {
-                            ingredientQuantArr
+                    //CANNOT FIND A SOULTION 
+                    Console.ForegroundColor = ConsoleColor.White;
+                    for (int i = 0; i < ingredientQuantArr.Length; i++)
+                    {
+                        
                         }
-                    
+
                 } else if (function == 5)
                 {
-                        //Given arrays have their values reset by having their indexes reduced to zero so there are no values within them
-                        ingredientNamesArr = new string[0];
-                        recipeStepsArr = new string[0];
-                        ingredientQuantArr = new double[0];
-                        ingredientUnitOfMeas = new string[0];
-                        //Message confirming
-                        Conole.WriteLine("Items cleared");
-                        Console.ReadLine();
-                    } else if (function == 6)
+                    
+                } else if (function == 6)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
-                        //Simple goodbye exit message with an enviroment exit method
+                    //Simple goodbye exit message with an enviroment exit method
                     Console.WriteLine("Goodbye");
-                    Console.ReadLine();
+
                     Environment.Exit(0);
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Input not valid");
-                    Console.ReadLine();
+
                 }
-                
+
+
+                }
 
             }
 
-        }
+        
     }
-}
+    }
